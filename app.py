ece141ae3c4dd9928eca1775cd85f1e120ad3217
Harsh <77411358+hershey-harsh@users.noreply.github.com>
2023-11-13 17:50:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 import turtle
 import random
+from config import min_x, max_x, min_y, max_y, bgcolor, speed
+from helpers.helpers import draw_random_angle_quadrilateral
+
+screen = turtle.Screen()
+screen.bgcolor(bgcolor)
+
+turtle.speed(int(speed))
+
+while turtle.xcor() < max_x and turtle.ycor() < max_y:
+    draw_random_angle_quadrilateral(min_x, max_x, min_y, max_y)
+
+turtle.hideturtle()
+turtle.done()
