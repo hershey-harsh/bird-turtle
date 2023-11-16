66333e84c51ca69e22b6a70421fe077120b8be3e
Harsh <77411358+hershey-harsh@users.noreply.github.com>
2023-11-16 17:59:38 -0500

    Update helpers.py

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -3,16 +3,20 @@ from helpers.colors import colors
 
 def draw_random_angle_quadrilateral(turtle, min_x, max_x, min_y, max_y):
     turtle.color(random.choice(colors))
-    turtle.begin_fill()
-    
+
+    if random.choice([True, False]):
+        turtle.begin_fill()
+
     side_lengths = [random.randint(50, 100) for _ in range(4)]
+
     rotation_angles = [random.randint(0, 360) for _ in range(4)]
 
     for i in range(4):
         turtle.forward(side_lengths[i])
         turtle.left(rotation_angles[i])
 
-    turtle.end_fill()
+    if random.choice([True, False]):
+        turtle.end_fill()
 
     turtle.penup()
     turtle.goto(random.uniform(min_x, max_x), random.uniform(min_y, max_y))
